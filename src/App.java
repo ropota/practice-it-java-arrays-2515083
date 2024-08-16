ff4cd780a9855520582d53d483336563e3bc6368
ropota <66786266+ropota@users.noreply.github.com>
2024-08-16 09:48:30 +0000

    implement triangle

--- src/App.java
+++ src/App.java
@@ -1,6 +1,27 @@
+import java.util.Arrays;
+
 public class App {
-  
-  public static void main(String[] args) {
-    
+  //{A, B, C}
+  //A
+  //AB
+  //ABC
+  public static void printTriangle(Object[] arr) {
+   for (int row = 0; row < arr.length; row++) {
+    for (int col = 0; col<=row; col++){
+    System.out.print(arr[col]);
+   }
+   System.out.println();
   }
 }
+
+  public static void main(String[] args) {
+    printTriangle(new Integer[] {1,2,3,4});
+    printTriangle(new Character[] {'A','B','C','D'});
+    printTriangle(new String[]{});
+    printTriangle(new String[] {"Hello"});
+    printTriangle(new String[]{"Hello","There","Friend"});
+ 
+  }
+}
+
+
